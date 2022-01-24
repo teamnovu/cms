25af57c450092fed53977ac33a1f7a16df34e38c
Manuel Strebel <manuel.strebel@novu.ch>
2022-01-24 13:37:29 +0100

    Revert "return only published entries by default"
    This reverts commit 0a03a8ea07a235e35e5af8ebf7175974a43f6f8a.

--- src/GraphQL/Queries/EntryQuery.php
+++ src/GraphQL/Queries/EntryQuery.php
@@ -6,15 +6,10 @@ use GraphQL\Type\Definition\Type;
 use Statamic\Facades;
 use Statamic\Facades\GraphQL;
 use Statamic\GraphQL\Types\EntryInterface;
-use Statamic\GraphQL\Types\JsonArgument;
-use Statamic\Support\Arr;
 use Statamic\Query\LivePreviewQueryBuilder;
-use Statamic\Tags\Concerns\QueriesConditions;
 
 class EntryQuery extends Query
 {
-    use QueriesConditions;
-
     protected $attributes = [
         'name' => 'entry',
     ];
@@ -32,7 +27,6 @@ class EntryQuery extends Query
             'collection' => GraphQL::string(),
             'uri' => GraphQL::string(),
             'site' => GraphQL::string(),
-            'filter' => GraphQL::type(JsonArgument::NAME),
         ];
     }
 
@@ -65,33 +59,6 @@ class EntryQuery extends Query
             $query->where('site', $site);
         }
 
-        $this->filterQuery($query, $args['filter'] ?? []);
-
         return $query->limit(1)->get()->first();
-    }
-
-    private function filterQuery($query, $filters)
-    {
-        if (!isset($filters['status']) && !isset($filters['published'])) {
-            $filters['status'] = 'published';
-        }
-
-        foreach ($filters as $field => $definitions) {
-            if (!is_array($definitions)) {
-                $definitions = [['equals' => $definitions]];
-            }
-
-            if (Arr::assoc($definitions)) {
-                $definitions = collect($definitions)->map(function ($value, $key) {
-                    return [$key => $value];
-                })->values()->all();
-            }
-
-            foreach ($definitions as $definition) {
-                $condition = array_keys($definition)[0];
-                $value = array_values($definition)[0];
-                $this->queryCondition($query, $field, $condition, $value);
-            }
-        }
     }
 }
