999541a07ff0211960ba36af2fcf9d58e08dd1f4
Nattha <nattha.khamso@novu.ch>
2022-05-20 17:41:03 +0200

    remove console.log

--- resources/js/components/live-preview/LivePreview.vue
+++ resources/js/components/live-preview/LivePreview.vue
@@ -364,10 +364,10 @@ export default {
             const iframeUrl = container.firstChild.src
             const targetOrigin = /^https?:\/\//.test(iframeUrl) ? (new URL(iframeUrl))?.origin : window.origin;
 
-            // console.log({
-            //     fieldIdentifier,
-            //     normalizedIdentifier,
-            // })
+            console.log({
+                fieldIdentifier,
+                normalizedIdentifier,
+            })
 
            container.firstChild.contentWindow.postMessage(
                 {
@@ -381,6 +381,8 @@ export default {
             const lpEditorSidebar = document.querySelector('.live-preview-editor')
 
             if (!lpEditorSidebar) return
+
+            console.log('registering focus event')
 
             lpEditorSidebar.addEventListener(
                 'focus',
